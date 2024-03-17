2430905d5606aea2649e3ab680793ab0025de189
DharmaAlamsyah <dharmadasa138@gmail.com>
2024-03-17 23:44:29 +0700

    create layout and view

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/home', 'Pages::home');
+$routes->get('/about', 'Pages::about');
+
+$routes->setAutoRoute(true);
---
+++ app/Views/pages/about.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="rows">
+        <div class="col">
+            <h1>Hubungi Kami</h1>
+        </div>
+    </div>
+</div>
